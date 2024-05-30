20914c27e72df7274933368fae297d788ede5812
Ian Dahlstrom <ian.dahlstrom@gmail.com>
2024-05-30 08:58:58 -0400

    wip: Added initial PPO implementation

---
+++ src/polyscape/api/train/ppo.py
@@ -0,0 +1,220 @@
+"""PPO Trainer"""
+
+# -------------------------------------------------------------------------
+#   Exports
+# -------------------------------------------------------------------------
+
+__all__: list[str] = ["train"]
+
+
+# -------------------------------------------------------------------------
+#   Imports
+# -------------------------------------------------------------------------
+
+# Standard Library Imports
+from functools import partial
+import secrets
+from typing import Any
+
+# External Imports
+import gymnasium as gym
+import jax
+from jax import numpy as jnp
+from jaxtyping import Array, Float, Int, PRNGKeyArray, PyTree
+import tqdm
+from tensorboardX import SummaryWriter  # type: ignore
+
+# Internal Imports
+from polyscape.typing import (
+    ActorCriticForwardFn,
+    OptimizerStepFn,
+    LearningRateScheduleFn,
+    ObsType,
+)
+
+
+# -------------------------------------------------------------------------
+#   Exported Functions
+# -------------------------------------------------------------------------
+
+
+@partial(jax.value_and_grad, argnums=1, has_aux=True)
+def episode_objective_fn(
+    agent_fn: ActorCriticForwardFn,
+    params: PyTree[Float[Array, "..."], "A"],
+    trajectory: PyTree[Array, "T"],
+    rng: PRNGKeyArray,
+    clipping_bound: float = 0.2,
+    discount_factor: float = 0.99,
+    gae_factor: float = 0.95,
+    value_fn_factor: float = 1.0,
+    entropy_fn_factor: float = 0.0,
+) -> tuple[PyTree[Float[Array, "..."], "A"], dict[str, Array]]:
+    k1, k2 = jax.random.split(rng, 2)
+    obs_, actions_, rewards_, next_obs_, old_probs_a_, masks_ = zip(*trajectory)
+
+    obs = tuple([jnp.stack(s) for s in zip(*obs_)])
+    actions: Int[Array, "horizon"] = jnp.stack(actions_)
+    rewards: Float[Array, "horizon"] = jnp.stack(rewards_).reshape(-1, 1)
+    old_probs_a = jnp.stack(old_probs_a_)
+    masks = jnp.stack(masks_, dtype=jnp.float32).reshape(-1, 1)
+    next_obs = tuple([jnp.stack(s) for s in zip(*next_obs_)])
+
+    pi, v = agent_fn(params, obs, rng=k1)
+    _, next_v = agent_fn(params, next_obs, rng=k2)
+
+    td_targets = rewards + (discount_factor * next_v * (jnp.ones(()) - masks))
+    deltas = td_targets - v
+    _, advantages = jax.lax.scan(
+        f=partial(_advantage_loop_body, scale=discount_factor * gae_factor),
+        init=jnp.zeros((1,), dtype=deltas.dtype),
+        xs=jnp.flip(deltas, 0),
+    )
+    advantages = jnp.flip(advantages, 0)
+
+    new_probs = jax.nn.softmax(pi, -1)
+    new_probs_a = new_probs[jnp.arange(pi.shape[0]), actions]
+    ratios = jnp.exp(jnp.log(new_probs_a) - jnp.log(old_probs_a)).reshape(-1, 1)
+    surrogate = -jnp.minimum(
+        ratios * advantages,
+        jnp.clip(ratios, 1 - clipping_bound, 1 + clipping_bound) * advantages,
+    ).mean()
+    value_fn_loss = value_fn_factor * jnp.power(v - td_targets, 2).mean()
+    entropy = jax.scipy.special.entr(new_probs).mean()
+    entropy_loss = entropy_fn_factor * entropy
+
+    loss = surrogate + value_fn_loss + entropy_loss
+    info = {
+        "loss": loss,
+        "value-fn-loss": value_fn_loss,
+        "entropy": entropy,
+        "total-horizon-rewards": jnp.sum(rewards),
+        "horizon-timesteps": jnp.array(actions.shape[0]),
+    }
+
+    return loss, info
+
+
+def _advantage_loop_body(advantage, delta, scale):
+    advantage = scale * advantage + delta
+    return advantage, advantage
+
+
+def generate_trajectory(
+    agent_fn: ActorCriticForwardFn,
+    params: PyTree[Float[Array, "..."], "A"],
+    env: gym.Env,
+    state: tuple[ObsType, float, bool, bool, dict[str, Any]],
+    horizon_timesteps: int,
+    seed: int,
+    epsilon: float = 1e-8,
+) -> tuple[int, PyTree[Array, "T"], ObsType]:
+    rng = jax.random.key(seed)
+    t = 0
+    obs, *_ = state
+
+    pbar = tqdm.trange(horizon_timesteps, leave=False, desc="trajectory")
+    trajectory = []
+    while t < horizon_timesteps:
+        kfn, kpi, rng = jax.random.split(rng, 3)
+        pi, v = agent_fn(params, obs, rng=kfn)
+        action = jax.random.categorical(kpi, pi)
+        prob = jax.nn.softmax(pi + epsilon, -1)[action]
+
+        next_obs, reward, finished, truncated, _ = env.step(action)
+        done = finished or truncated
+        t += 1
+
+        trajectory.append((obs, action, reward, next_obs, prob, done))
+        obs = next_obs
+
+        pbar.update()
+
+        if done:
+            break
+
+    pbar.close()
+
+    return t, trajectory, obs
+
+
+def train(
+    agent_fn: ActorCriticForwardFn,
+    params: PyTree[Float[Array, "..."], "A"],
+    optimizer_fn: OptimizerStepFn,
+    optstate: PyTree[Float[Array, "..."], "O"],
+    lr_schedule_fn: LearningRateScheduleFn,
+    env: gym.Env,
+    logger: SummaryWriter | None = None,
+    total_timesteps: int = 10_000,
+    horizon_timesteps: int = 64,
+    epochs_per_trajectory: int = 3,
+    clipping_epsilon: float = 0.2,
+    discount_factor: float = 0.99,
+    gae_factor: float = 0.95,
+    value_fn_factor: float = 1.0,
+    entropy_fn_factor: float = 0.0,
+    epsilon: float = 1e-8,
+    seed: int | None = None,
+) -> PyTree[Float[Array, "..."], "A"]:
+    seed = secrets.randbits(30) if seed is None else seed
+    obs, info = env.reset(seed=seed)
+    rng = jax.random.key(seed)
+
+    pbar = tqdm.trange(total_timesteps, desc="timesteps", leave=True)
+    global_step = 0
+    optstep = 0
+    while global_step < total_timesteps:
+        steps_taken, trajectory, obs = generate_trajectory(
+            agent_fn=agent_fn,
+            params=params,
+            env=env,
+            state=(obs, 0.0, False, False, info),
+            horizon_timesteps=horizon_timesteps,
+            seed=seed + global_step,
+            epsilon=epsilon,
+        )
+
+        metrics: dict[str, Float[Array, ""]] = NotImplemented
+        for epoch in range(epochs_per_trajectory):
+            key, rng = jax.random.split(rng, 2)
+            (_, logdict), grads = episode_objective_fn(
+                agent_fn,
+                params,
+                trajectory=trajectory,
+                rng=key,
+                clipping_bound=clipping_epsilon,
+                discount_factor=discount_factor,
+                gae_factor=gae_factor,
+                value_fn_factor=value_fn_factor,
+                entropy_fn_factor=entropy_fn_factor,
+            )
+
+            params, optstate = optimizer_fn(
+                params=params,
+                grads=grads,
+                optstate=optstate,
+                step=optstep,
+                lr=lr_schedule_fn(optstep),
+            )
+
+            if epoch == 0:
+                metrics = logdict
+
+            else:
+                for key, val in logdict.items():
+                    metrics[key] += (val - metrics[key]) / (epoch + 1)
+
+            optstep += 1
+
+        if logger is not None:
+            for key, val in metrics.items():
+                logger.add_scalar(key, jnp.array(val).item(), global_step)
+
+        global_step += steps_taken
+        pbar.update(steps_taken)
+
+        if steps_taken < horizon_timesteps:
+            obs, info = env.reset(seed=seed + global_step)
+
+    pbar.close()
--- src/polyscape/cli/train.py
+++ src/polyscape/cli/train.py
@@ -15,7 +15,7 @@ import jax
 from tensorboardX import SummaryWriter  # type: ignore
 
 # Internal Imports
-from polyscape.api.train import reinforce, reinforce_with_baseline
+from polyscape.api.train import reinforce, reinforce_with_baseline, ppo
 from polyscape.agents.v0.mlp import actor_critic_agent_init
 from polyscape.cli.utils import add_environment_arguments
 from polyscape.optim import adam, schedules
@@ -31,6 +31,7 @@ def add_arguments(parser: ArgumentParser):
     algos = parser.add_subparsers(title="Algorithm")
     add_reinforce_arguments(algos.add_parser("reinforce"))
     add_reinforce_with_baseline_arguments(algos.add_parser("reinforce-with-baseline"))
+    add_ppo_arguments(algos.add_parser("ppo"))
 
 
 # -------------------------------------------------------------------------
@@ -94,6 +95,40 @@ def add_reinforce_with_baseline_arguments(parser: ArgumentParser):
         algo = p.add_argument_group("REINFORCE Hyperparameters")
         algo.add_argument("--total-timesteps", type=int, default=1000)
         algo.add_argument("--discount-factor", type=float, default=0.99)
+
+    agent_subparsers = parser.add_subparsers(title="Agent Version")
+    v0: ArgumentParser = agent_subparsers.add_parser("v0")
+    add_experiment_arguments(v0)
+    add_environment_arguments(v0)
+    add_actor_critic_v0_arguments(v0)
+    add_args(v0)
+
+
+def add_ppo_arguments(parser: ArgumentParser):
+    parser.set_defaults(
+        train_fn_factory=lambda args: partial(
+            ppo.train,
+            total_timesteps=args.total_timesteps,
+            horizon_timesteps=args.horizon_timesteps,
+            epochs_per_trajectory=args.epochs_per_trajectory,
+            clipping_epsilon=args.clipping_epsilon,
+            discount_factor=args.discount_factor,
+            gae_factor=args.gae_factor,
+            value_fn_factor=args.value_fn_factor,
+            entropy_fn_factor=args.entropy_fn_factor,
+        )
+    )
+
+    def add_args(p: ArgumentParser):
+        algo = p.add_argument_group("PPO Hyperparameters")
+        algo.add_argument("--total-timesteps", type=int, default=10000)
+        algo.add_argument("--horizon-timesteps", type=int, default=64)
+        algo.add_argument("--epochs-per-trajectory", type=int, default=3)
+        algo.add_argument("--clipping-epsilon", type=float, default=0.2)
+        algo.add_argument("--discount-factor", type=float, default=0.99)
+        algo.add_argument("--gae-factor", type=float, default=0.95)
+        algo.add_argument("--value-fn-factor", type=float, default=1.0)
+        algo.add_argument("--entropy-fn-factor", type=float, default=0.0)
 
     agent_subparsers = parser.add_subparsers(title="Agent Version")
     v0: ArgumentParser = agent_subparsers.add_parser("v0")
